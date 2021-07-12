c65e23b7635ab9afaa3d33e87f61d1ff4e2dc31d
Lukas Jesche <ljaschka@gmail.com>
2021-07-13 00:08:58 +0200

    - mongo

--- src/mongodb_connector.py
+++ src/mongodb_connector.py
@@ -1,43 +1,50 @@
-from kafka import KafkaConsumer, KafkaProducer
+import time
+
 import pymongo
 import logging
-import json
+
+from event_stream.event_stream_consumer import EventStreamConsumer
 
 
-def main(config):
-    logging.warning('MongoDB topic %r in collection %s, started' % (
-        config['topic'], config['mongo_collection']))
-    mongoClient = pymongo.MongoClient(config['mongo_url'])
-    db = mongoClient[config['mongo_client']]
-    collection = db[config['mongo_collection']]
+class MongoDBConnector(EventStreamConsumer):
+    state = ["linked"]
+    relation_type = "discusses"
 
-    consumer = KafkaConsumer(config['topic'], group_id=config['group_id'],
-                             bootstrap_servers=config['bootstrap_servers'], api_version=(0, 10),
-                             consumer_timeout_ms=5000)
+    log = "MongoDB Connector "
+    group_id = "mongo_db_connector_lpd"
 
-    while True:
+    mongoClient = False
+
+    config = {
+        'mongo_url': "mongodb://mongo_db:27017/",
+        'mongo_client': "events",
+        'mongo_collection': "dev",
+    }
+
+    def __init__(self, id):
+        super().__init__(id)
+        # todo
+        self.mongoClient = pymongo.MongoClient(host=self.config['mongo_url'],
+                                               serverSelectionTimeoutMS=3000,  # 3 second timeout
+                                               # username="root",
+                                               # password="example"
+                                               )
+        db = self.mongoClient[self.config['mongo_client']]
+        self.collection = db[self.config['mongo_collection']]
+
+    def on_message(self, json_msg):
+        logging.warning(self.log + "on message mongo consumer")
+
         try:
-            for msg in consumer:
-                logging.warning('MongoDB topic %r in collection %s, write' % (
-                    config['topic'], config['mongo_collection']))
-                json_response = json.loads(msg.value)
-                collection.insert_one(json_response)
+            self.collection.insert_one(json_msg)
         except pymongo.errors.DuplicateKeyError:
             logging.warning("MongoDB topic %r in collection %s, Duplicate found, continue" % (
-            config['topic'], config['mongo_collection']))
-        except Exception as exc:
-            consumer.close()
-            logging.error('MongoDB topic %r in collection %s, generated an exception: %s' % (
-            config['topic'], config['mongo_collection'], exc))
-            break
+                self.config['topic'], self.config['mongo_collection']))
+
 
 if __name__ == '__main__':
-    config = {
-        'mongo_url': "mongodb://localhost:27017/",
-        'mongo_client': "TweetTest",
-        'mongo_collection': "test_link",
-        'topic': 'linked',
-        'group_id': 'mongo-db-consumer-linked',
-        'bootstrap_servers': 'localhost:9092',
-    }
-    main(config=config)
+    logging.warning("start Mongo DB connector")
+    time.sleep(15)
+
+    e = MongoDBConnector(1)
+    e.consume()
